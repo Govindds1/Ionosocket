2a343ccf1393a9dc682b4d4de54b3b4aa70c8cdb
Govind Raj <govindrajgaya786@gmail.com>
2025-06-17 15:56:28 +0530

    express setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static('public'));
+
+server.listen(9000, () => {
+  console.log('Server is listening on port 9000');
+});
