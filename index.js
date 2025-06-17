8fc804f790001b2ad019338c6d079b4b3a57226d
Govind Raj <govindrajgaya786@gmail.com>
2025-06-17 16:36:34 +0530

    socket connected

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
+const { Server } = require("socket.io");
+
+
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
+// Handle socket connections
 
+io.on('connection', (socket) => {
+  console.log('A user connected', socket.id);
+});
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
